bf32b8710c4c3ce40e59adf0486902b5076efb99
José Ivan R. de Oliveira <ivan_r.o@hotmail.com>
2024-03-13 11:56:38 -0300

    feat: add simpe test to run actions

--- app/tests/Fiap.TasteEase.Domain.Tests/UnitTest1.cs
+++ app/tests/Fiap.TasteEase.Domain.Tests/UnitTest1.cs
@@ -5,7 +5,16 @@ namespace Fiap.TasteEase.Domain.Tests
         [Fact]
         public void Test1()
         {
+            // Arrange
+            var a = 1;
+            var b = 2;
+            var expected = 3;
 
+            // Act
+            var result = a + b;
+
+            // Assert
+            Assert.Equal(expected, result);
         }
     }
 }
