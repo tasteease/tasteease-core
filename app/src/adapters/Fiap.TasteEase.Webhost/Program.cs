c3f629f899068b9ae0c5539bf56d9b126cb85025
André Ribeiro <andrebnu1@gmail.com>
2024-03-18 20:52:38 -0300

    feat: enable swagger

--- app/src/adapters/Fiap.TasteEase.Webhost/Program.cs
+++ app/src/adapters/Fiap.TasteEase.Webhost/Program.cs
@@ -16,12 +16,8 @@ builder.Services.AddEfCoreConfiguration(builder.Configuration);
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseHttpsRedirection();
 app.UseAuthorization();
